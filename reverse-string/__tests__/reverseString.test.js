2262a011f86e2b54ac883552825e1b810d33bd4c
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com.>
2025-02-19 09:28:02 +0100

    feat: add other tests for reverse string
    complete exercise

--- reverse-string/__tests__/reverseString.test.js
+++ reverse-string/__tests__/reverseString.test.js
@@ -3,3 +3,27 @@ const reverseString = require("../reverseString");
 test("String reverses", () => {
   expect(reverseString("hello")).toBe("olleh");
 });
+
+test("String reverses with uppercase", () => {
+  expect(reverseString("Hello")).toBe("olleH");
+});
+
+test("Empty string", () => {
+  expect(reverseString("")).toBe("");
+});
+
+test("Single character", () => {
+  expect(reverseString("a")).toBe("a");
+});
+
+test("Palindrome", () => {
+  expect(reverseString("racecar")).toBe("racecar");
+});
+
+test("Special characters", () => {
+  expect(reverseString("!@#$%^&*()")).toBe(")(*&^%$#@!");
+});
+
+test("Whitespace", () => {
+  expect(reverseString("  hello  ")).toBe("  olleh  ");
+});
